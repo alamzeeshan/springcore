4bc48d8c67c7bd1733e78f16093cf895b189a9f7
alamzeeshan <zeeshan.tcs@gmail.com>
2016-08-26 21:38:52 +0530

    Initial Commit

---
+++ src/main/java/springcore/MessageService.java
@@ -0,0 +1,5 @@
+package springcore;
+
+public interface MessageService {
+    String getMessage();
+}
---
+++ src/main/java/springcore/MessageServiceImpl.java
@@ -0,0 +1,9 @@
+package springcore;
+
+public class MessageServiceImpl implements MessageService {
+
+	public String getMessage() {
+        return "Hello World!";
+      }
+
+}
